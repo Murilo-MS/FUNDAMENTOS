b628300419a7ebc741b5a604560e6336c7986533
Murilo de Moraes Sigismundo <85578691+Murilo-MS@users.noreply.github.com>
2022-03-18 18:51:53 -0300

    Adding new classes

---
+++ type-number.js
@@ -0,0 +1,19 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const secundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + secundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const PontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
