0ae0e99cea214b3b0c57a2d06c6e605a3a018b69
BaconTunado <85578691+BaconTunado@users.noreply.github.com>
2022-03-19 15:20:24 -0300

    Adding funcions and parameters

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+// let x = "";
+// console.log(x)
+// x = "oi"; 
+
+
+// 1) Declara a função
+
+ function imprimeTexto(texto) {
+     console.log(texto)
+}
+
+// // 2) Executa a função
+
+ imprimeTexto(soma());
+
+// três formas de escrever funções:
+
+function soma(){
+//  const resultado = 2 + 2
+    return 2 + 2;
+}
+
+//console.log(soma())
+
---
+++ parametros.js
@@ -0,0 +1,26 @@
+// parâmetros de função
+
+function soma(num1, num2){
+    return num1 + num2;
+}
+
+// console.log(soma(2,2));
+// console.log(soma(245,20));
+// console.log(soma(-500,60));
+
+// parâmetros x argumentos
+
+// ordem dos parâmetros
+
+function nomeIdade(nome, idade){
+    return `meu nome é ${nome} e minha idade é ${idade}`
+}
+
+// console.log(nomeIdade(40, 'Juliana'))
+
+function multiplica(num1 = 1, num2 = 1) {
+    return num1 * num2;
+}
+
+console.log(multiplica(soma(4,5), soma (3,3)));
+
