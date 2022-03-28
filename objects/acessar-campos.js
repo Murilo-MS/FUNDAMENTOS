22b5841965e46b1856b79888c04626340fa7a9ab
BaconTunado <85578691+BaconTunado@users.noreply.github.com>
2022-03-28 19:44:35 -0300

    Add for each object interaction class

---
+++ objects/acessar-campos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome:"André",
+    idade:36,
+    cpf:"12345667899",
+    email:"andre@gmail.com",
+    contato:["5591235498","5521988743124"]
+}
+
+cliente.contato.forEach(fone => console.log(fone));
